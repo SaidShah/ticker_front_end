692ecb9e2d6d99c98eb8548ad5c3a8d50fed1454
said shah <saidmysr20@aol.com>
2018-12-28 21:15:16 -0500

    added a marketplace container

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../App.css";
 
 class Marketplace extends Component {
-
   render() {
     return (
-      <div>
-        <h1 className="up">this is the Marketplace</h1>
-      </div>
+      <>
+        <div className="card cardBoarder">
+          <div className="card-header card-title  center-text">
+            Symbol: {this.props.stock.symbol}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Special title treatment</h5>
+            <p className="card-text">
+              It's a broader card with text below as a natural lead-in to extra
+              content. This content is a little longer.
+            </p>
+            <a href="#" className="btn btn-primary cardBoarder cardBtn">
+              view {this.props.stock.symbol}
+            </a>
+          </div>
+        </div>
+      </>
     );
   }
-
 }
 
 export default Marketplace;
--- src/containers/MarketplaceContainer.js
+++ src/containers/MarketplaceContainer.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import Marketplace from '../components/Marketplace'
+import '../App.css'
 
 class MarketplaceContainer extends Component {
+  state={
+    popularStocks: ["GOOG","TSLA","MSFT","AAPL","AMZN","EBAY","NKE","PFE","BAC","NOK","INTC","C","GE","FOX","T","WFC",
+              "RAD","VZ","ORCL","MS","TWTR","BABA","XOM","KO","MRK","ABX","GM","INFY","HES","DIS","GG","MET","HAL","WMT",
+              "CVX","BB","X","BX","DAL","MGM","AIG","JCI","HPE","ATUS","WPX","CVS"],
+    stocksToShow: []
+  }
+
+  componentDidMount() {
+    let symbols = this.getRandomStocks().join(",")
+    fetch(`https://api.robinhood.com/quotes/?symbols=${symbols}`)
+    .then(res => res.json())
+    .then(stocks =>{
+      this.setState({
+        stocksToShow: stocks.results
+      })
+    })
+  }
+
+  getRandomStocks=()=>{
+    let indexes = []
+    let stocks = [...this.state.popularStocks]
+    for(let i = 0; i < 15; i++){
+      let randomNum = Math.floor(Math.random()*this.state.popularStocks.length)
+      indexes.push(randomNum)
+    }
+     let uniqueIndexes = Array.from(new Set(indexes))
+     let allStocks = []
+     for(let index of uniqueIndexes){
+       allStocks.push(stocks[index])
+     }
+     return allStocks
+  }
+
+  viewStock=(e,stock)=>{
+    console.log(stock);
+  }
 
   render() {
+    let stockArr = this.state.stocksToShow.map(eachStock =>{
+       return <Marketplace stock={eachStock} viewStock={this.viewStock} key={eachStock.symbol}/>
+    })
     return (
-      <div>
-        <Marketplace/>
+      <div className="center-card">
+      <h1 className="box-header-text"> Welcome to the Marketplace </h1>
+      <h2 className="box-header-text">Trending Stocks</h2>
+        {stockArr}
       </div>
     );
   }
