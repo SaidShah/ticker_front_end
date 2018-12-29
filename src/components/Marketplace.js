87dfb5487789f7e6d7a5fb346dca9f7b58a776da
said shah <saidmysr20@aol.com>
2018-12-28 21:29:59 -0500

    added cards to the marketplace component

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -3,18 +3,20 @@ import "../App.css";
 
 class Marketplace extends Component {
   render() {
+    console.log(this.props.stock);
     return (
       <>
         <div className="card cardBoarder">
-          <div className="card-header card-title  center-text">
+          <div className="card-header card-title center-text">
             Symbol: {this.props.stock.symbol}
           </div>
           <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              It's a broader card with text below as a natural lead-in to extra
-              content. This content is a little longer.
+            <h4 className="card-title">Current Price: $ {parseFloat(this.props.stock.last_trade_price).toFixed(2)}</h4>
+            <p className="card-text bolden-text">
+              <span> Previous Close: $ {parseFloat(this.props.stock.adjusted_previous_close).toFixed(2)} </span>
+              | <span> Asking Price: $ {parseFloat(this.props.stock.ask_price).toFixed(2)} </span>
             </p>
+            <p className="card-text bolden-text">Last extended hours trade price: $ {parseFloat(this.props.stock.last_extended_hours_trade_price).toFixed(2)}</p>
             <a href="#" className="btn btn-primary cardBoarder cardBtn">
               view {this.props.stock.symbol}
             </a>
